89b7616bdc3b05e49eb2bc21627ff19426f4ac07
Andrez Ramirez <andrezzz2312@gmail.com>
2022-04-29 14:41:55 -0500

    f

--- 1-basic.js
+++ 1-basic.js
@@ -21,6 +21,7 @@ const menu = [
     category: 'shakes',
     price: 6.99,
     img: './images/item-3.jpeg',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
     id: 4,
