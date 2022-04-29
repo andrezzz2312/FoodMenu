4fe056d7d5b1ae4babcdab56048ad7f7aedaf4f6
Andrez Ramirez <andrezzz2312@gmail.com>
2022-04-29 14:39:03 -0500

    m

--- 1-basic.js
+++ 1-basic.js
@@ -13,6 +13,7 @@ const menu = [
     category: 'lunch',
     price: 13.99,
     img: './images/item-2.jpeg',
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
     id: 3,
@@ -20,7 +21,6 @@ const menu = [
     category: 'shakes',
     price: 6.99,
     img: './images/item-3.jpeg',
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
     id: 4,
